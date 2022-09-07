4767ea4e4d2bf5b152df570c69de62dabe0508f4
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2022-09-07 09:43:22 +0200

    Fixed multiple download buttons issue & added color option

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btn = document.querySelector('.submit')
-
-
+let randomNum = 1
 
 btn.addEventListener('click', () => {
     const canvas = document.getElementById('canvas')
@@ -11,6 +10,12 @@ btn.addEventListener('click', () => {
     const height = document.querySelector('.height')
     const keyword = document.querySelector('.keyword').value
     const author = document.querySelector('.author').value
+    const hexColor = document.querySelector('.hexcolor').value
+    const btnDown = document.querySelector('.btn-download')
+
+    if (btnDown != null){
+        btn.parentNode.removeChild(btnDown);
+    }
 
     let gradient = ctx.createLinearGradient(0, 0, 170, 100);
     gradient.addColorStop(0, "rgb(255, 0, 128)");
@@ -18,39 +23,47 @@ btn.addEventListener('click', () => {
 
     // img.src = "https://source.unsplash.com/random/50×50"
     
-
-    console.log(keyword)
-    
     canvas.width = '800'
     canvas.height = '600'
 
     let img = new Image()
     img.crossOrigin="anonymous"
+    
     if (keyword.length == 0){
-        img.src = 'https://source.unsplash.com/random/1000x1000'
+        img.src = `https://source.unsplash.com/random/1000x1000&${randomNum}`
     } else {
-        img.src = `https://source.unsplash.com/1600x900/?${keyword}`
+        img.src = `https://source.unsplash.com/1600x900/?${keyword}&${randomNum}`
     }
+
+    randomNum ++
     
     wrappedText = getLines(ctx, quote, 75, 75, 175, 40)
     
     img.addEventListener("load", ()=>{
         ctx.drawImage(img,0,0);
         ctx.font = '35px serif';
-        ctx.fillStyle = gradient;
+
+        if (hexColor.length != 0){
+            ctx.fillStyle = hexColor 
+        } else {
+            ctx.fillStyle = gradient
+        }
+
+        
         wrappedText.forEach(function(item) {
-            ctx.fillText(item[0], item[1], item[2]); 
+            ctx.fillText(item[0], item[1], item[2]);
         })
 
         if (author.length != 0){
             const lastLine = wrappedText[wrappedText.length - 1][2]
-            ctx.fillText(`- ${author}`, 200, lastLine + 60); 
+            ctx.fillText(`- ${author}`, 200, lastLine + 60)
         }
     })
 
     const btnDownload = document.createElement('button')
     btnDownload.innerHTML = '<i class="fa fa-download"></i> Download'
     btnDownload.classList.add('btn')
+    btnDownload.classList.add('btn-download')
 
     container.appendChild(btnDownload)
 
@@ -68,14 +81,12 @@ btn.addEventListener('click', () => {
 })
 
 
-// fix text colour - gradient
+
 // css
 // Validation to ensure data is given
 // reset after quote generated
 // change place of down btn
-// make sure 1 down button is created
 // allow text size options
-// colour options
 
 function getLines(ctx, text, x, y, maxWidth, lineHeight) {
     const words = text.split(" ");
