e50c8d0d30fa3be67443b4ca91e0152c500fa2ae
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2022-09-07 16:16:08 +0200

    API Access and general changes
    Major Change - using API access to get photos.
    Added default keyword
    Added data validation and text placeholder.
    Changed size and introduced orientations.
    Added link to original photo
    Changed layout of download buttons

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
-const btn = document.querySelector('.submit')
+const btn = document.querySelector('.gen-quote')
 let randomNum = 1
+const keyword = 'nature'
 
 btn.addEventListener('click', () => {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
     const container = document.querySelector('.container')
     const quote = document.querySelector('.quote').value
-    const width = document.querySelector('.width')
-    const height = document.querySelector('.height')
-    const keyword = document.querySelector('.keyword').value
+    const orientation = document.getElementById('orientation').value
+    const keywordEl = document.querySelector('.keyword').value
     const author = document.querySelector('.author').value
     const hexColor = document.querySelector('.hexcolor').value
+    const downloadEl = document.getElementById('downloadbtns')
     const btnDown = document.querySelector('.btn-download')
+    const btnDownOriginal = document.querySelector('.btn-download-original')
+
+    // const width = size.split("x")[0]
+    // const height = size.split("x")[1]
+
+    if (quote.length == 0){
+        return
+    }
 
     if (btnDown != null){
-        btn.parentNode.removeChild(btnDown);
+        btnDown.parentNode.removeChild(btnDown);
+    }
+    if (btnDownOriginal != null){
+        btnDownOriginal.parentNode.removeChild(btnDownOriginal);
     }
 
     let gradient = ctx.createLinearGradient(0, 0, 170, 100);
     gradient.addColorStop(0, "rgb(255, 0, 128)");
     gradient.addColorStop(1, "rgb(255, 153, 51)");
 
-    // img.src = "https://source.unsplash.com/random/50×50"
+    if (orientation == 'portrait'){
+        canvas.width = 600
+        canvas.height = 900
+    } else{
+        canvas.width = 900
+        canvas.height = 600
+    }
     
-    canvas.width = '800'
-    canvas.height = '600'
+    // canvas.width = width
+    // canvas.height = height
 
     let img = new Image()
     img.crossOrigin="anonymous"
     
-    if (keyword.length == 0){
-        img.src = `https://source.unsplash.com/random/1000x1000&${randomNum}`
-    } else {
-        img.src = `https://source.unsplash.com/1600x900/?${keyword}&${randomNum}`
+    
+    if (keywordEl.length != 0){
+        keyword = keywordEl
     }
+
+    const url = `https://api.unsplash.com/photos/random/?orientation=${orientation}&query=${keyword}&client_id=${clientId}`
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            img.src = data.urls.regular + `&w=${canvas.width}&h=${canvas.height}dpr=2`
+            imageLink = data.links.html
+            imageId = data.id
+            creator = data.user.name //create link to user
+            creator_link = data.user.portfolio_url
+        })
+        .catch(error => {
+            console.log('Error:' + error)
+        })
 
     randomNum ++
     
@@ -48,7 +80,6 @@ btn.addEventListener('click', () => {
         } else {
             ctx.fillStyle = gradient
         }
-
         
         wrappedText.forEach(function(item) {
             ctx.fillText(item[0], item[1], item[2]);
@@ -61,32 +92,38 @@ btn.addEventListener('click', () => {
     })
 
     const btnDownload = document.createElement('button')
-    btnDownload.innerHTML = '<i class="fa fa-download"></i> Download'
+    btnDownload.innerHTML = '<i class="fa fa-download"></i> Download Quote'
     btnDownload.classList.add('btn')
     btnDownload.classList.add('btn-download')
-
-    container.appendChild(btnDownload)
-
+    downloadEl.appendChild(btnDownload)
 
     btnDownload.addEventListener('click', () => {
         let canvasUrl = canvas.toDataURL()
         
         const createEl = document.createElement('a')
         createEl.href = canvasUrl
-        createEl.download = "download-this-canvas"
+        createEl.download = imageId
 
         createEl.click()
         createEl.remove()
     })
+
+    const btnDownloadOriginal = document.createElement('button')
+    btnDownloadOriginal.innerHTML = '<i class="fa fa-download"></i> Download Original Photo'
+    btnDownloadOriginal.classList.add('btn')
+    btnDownloadOriginal.classList.add('btn-download-original')
+    downloadEl.appendChild(btnDownloadOriginal)
+
+    btnDownloadOriginal.addEventListener('click', () => {
+        const createEl = document.createElement('a')
+        createEl.target = '_blank'
+        createEl.href = imageLink
+        createEl.click()
+        createEl.remove()
+    })
+
+    downloadEl.classList.remove('hidden')
 })
-
-
-
-// css
-// Validation to ensure data is given
-// reset after quote generated
-// change place of down btn
-// allow text size options
 
 function getLines(ctx, text, x, y, maxWidth, lineHeight) {
     const words = text.split(" ");
@@ -116,3 +153,24 @@ function getLines(ctx, text, x, y, maxWidth, lineHeight) {
     }
     return lineArray;
 }
+
+function checker() {
+    if(document.querySelector('.quote').value==="") { 
+        document.querySelector('.gen-quote').disabled = true; 
+       } else { 
+        document.querySelector('.gen-quote').disabled = false;
+       }
+   }
+
+
+// Change css of list options when expanded
+// Gice credit to user - next to where dowbnload button - first chnage whole layout of display
+// reset after quote generated
+// allow text size options
+// css
+// change place of down btn
+// Fixing canvas sizing issue in mobile
+// Design by hand how best to visualize
+
+// 1800 / 1200
+// 900 / 600
